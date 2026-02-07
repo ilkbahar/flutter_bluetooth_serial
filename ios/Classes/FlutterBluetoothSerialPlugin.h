88d65f79ef139e10a33f100aaf6ec037e24c756b
Mehmet Hasan İLKBAHAR <ilkbaharhasan5@gmail.com>
2026-02-07 13:22:17 +0200

    Fix import statement for Flutter header based on availability

--- ios/Classes/FlutterBluetoothSerialPlugin.h
+++ ios/Classes/FlutterBluetoothSerialPlugin.h
@@ -1,4 +1,8 @@
+#if __has_include(<Flutter/Flutter.h>)
 #import <Flutter/Flutter.h>
+#else
+#import "Flutter/Flutter.h"
+#endif
 
 @interface FlutterBluetoothSerialPlugin : NSObject<FlutterPlugin>
 @end
